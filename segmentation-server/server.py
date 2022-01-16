7d7ada03c559929c2b5a31d973c2641e4aa4f80a
Stevel705 <stevel705@outlook.com>
2022-01-16 11:58:53 +0300

    add param in docstring

--- segmentation-server/server.py
+++ segmentation-server/server.py
@@ -15,7 +15,10 @@ app = FastAPI(
 
 @app.post("/segmentation")
 def get_segmentation_mask(file: bytes = File(...)):
-    """Get segmentation mask from image file"""
+    """Get segmentation mask from image file
+        
+    - **file**: image of the olfactory bulb
+        """
     segmented_image = predict(file)
     bytes_io = io.BytesIO()
     segmented_image.save(bytes_io, format="PNG")
